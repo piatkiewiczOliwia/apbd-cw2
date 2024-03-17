1be2e1a4b0a91e2388e21678cf879d024ade0939
Oliwia Piątkiewicz <piatkiewicz.oliwia0606@gmail.com>
2024-03-17 20:28:10 +0100

    metoda CalculateAverage

--- app-cw2/app-cw2/Program.cs
+++ app-cw2/app-cw2/Program.cs
@@ -1,5 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Modyfikacja 1");
-Console.WriteLine("Modyfikacja 2");
-Console.WriteLine("Modyfikacja 3");
+public class Program
+{
+    public static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("array is empty");
+        }
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
